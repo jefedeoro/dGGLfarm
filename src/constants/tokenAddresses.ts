8f931f4bcde94d95747976a01c2853d062ec8022
jefedeoro <83683076+jefedeoro@users.noreply.github.com>
2021-08-14 20:36:40 -0500

    Update tokenAddresses.ts

--- src/constants/tokenAddresses.ts
+++ src/constants/tokenAddresses.ts
@@ -1,4 +1,4 @@
-export const sushi = '0x0e2298e3b3390e3b945a5456fbf59ecc3f55da16'
+export const sushi = '0x92a0E93dA7e898ad4E80b3E588590C23daF5CF8c'
 export const sushiv2 = '0xaba8cac6866b83ae4eec97dd07ed254282f6ad8a'
 export const sushiAddress = '0x43a7903E3a839a67192151eE300e11198985E54b'
-export const masterChefAddress = '0x245A074cA9814fB46A21562bC70fAB92F8A3F779'
+export const masterChefAddress = '0xEc33d3AbdC85Bb2eaa0CaD9C304c457BDEc38895'
